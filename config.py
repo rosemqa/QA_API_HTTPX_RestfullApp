81a0be28b5efdec6952e0f10462051f197588132
rosemqa <ers6wobmo09m@mail.ru>
2026-05-02 00:56:19 +0300

    changed  env_nested_delimiter to __

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8',
-        env_nested_delimiter='.'
+        env_nested_delimiter='__'
     )
 
     http_client: HTTPClientConfig
